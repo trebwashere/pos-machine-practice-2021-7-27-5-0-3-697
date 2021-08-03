244aaa0c3387e650ba516af5fcb8f83fc48265fb
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-03 23:28:39 +0800

    [STPosV2][Robert] Added getItemInformation method that gets the item information via barcode

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -2,8 +2,21 @@ package pos.machine;
 
 import java.util.List;
 
+import static pos.machine.ItemDataLoader.loadAllItemInfos;
+
+
 public class PosMachine {
-    public String printReceipt(List<String> barcodes) {
+    public static String printReceipt(List<String> barcodes) {
         return null;
     }
+
+    public static ItemInfo getItemInformation(String barCode) {
+        for(ItemInfo item : loadAllItemInfos()) {
+            if (item.getBarcode().equals(barCode)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
 }
