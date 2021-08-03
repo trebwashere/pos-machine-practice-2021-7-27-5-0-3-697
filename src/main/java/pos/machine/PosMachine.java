55a1997975f88a7cf8162a10b412ddb192fcaf0c
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-03 23:33:09 +0800

    [STPosV2][Robert] Added main method getItems that calls getItemInformation and getItemCount and call getItems in printReceipt

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,6 +1,8 @@
 package pos.machine;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 import static pos.machine.ItemDataLoader.loadAllItemInfos;
@@ -8,7 +10,25 @@ import static pos.machine.ItemDataLoader.loadAllItemInfos;
 
 public class PosMachine {
     public static String printReceipt(List<String> barcodes) {
+        List<Item> itemList = getItems(barcodes);
         return null;
+    }
+
+    public static List<Item> getItems(List<String> barcodeList) {
+        List<Item> itemList = new ArrayList<>();
+        List<String> barcodeLinkedList = new ArrayList<>(new LinkedHashSet<>(barcodeList));
+        for (String currentBarcode : barcodeLinkedList) {
+            ItemInfo itemInfo = getItemInformation(currentBarcode);
+            if (itemInfo != null) {
+                Item item = new Item(){{
+                    setName(itemInfo.getName());
+                    setUnitPrice(itemInfo.getPrice());
+                    setQuantity(getItemCount(currentBarcode,barcodeList));
+                }};
+                itemList.add(item);
+            }
+        }
+        return itemList;
     }
 
     public static ItemInfo getItemInformation(String barCode) {
