9200eaf1e5204565cfdb28e8a004add12ab49949
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-03 23:26:24 +0800

    [STPosV2][Robert] Added Items class and Receipt class as base objects for item information and receipt output

---
+++ src/main/java/pos/machine/Receipt.java
@@ -0,0 +1,24 @@
+package pos.machine;
+
+import java.util.List;
+
+public class Receipt {
+    private List<Item> itemList;
+    private int totalPrice;
+
+    public List<Item> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<Item> itemList) {
+        this.itemList = itemList;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
